fe14e8a9bd20646378dbb80ad0a6278be0830400
Paula Kamisato <paukamisato@gmail.com>
2022-06-05 12:42:40 +0200

    class to className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <div class="container">
+      <div className="container">
         <h5 className="container-title">Wiki Countries</h5>
       </div>
     </nav>
